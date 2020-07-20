e5aa705134f3933cfc1668a90d09cc8a04e9a37b
unknown <S10917@dorts.gov.tw>
2020-07-20 13:45:34 +0800

    加入專案檔案。

---
+++ prjFirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prjFirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ViewBag.DateTime = DateTime.Now.ToString(); 
+            //將目前日期時間轉成字串並存入ViewBag物件的DateTime動態屬性內，再指定回傳View到用戶端
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
